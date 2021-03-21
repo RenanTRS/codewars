0a47d875f3d71a80fb8bb66e0f251b610d2f9b30
RenanTRS <renantrsouza@gmail.com>
2021-03-21 16:22:48 -0400

    Comentários

--- python/shortestWord.py
+++ python/shortestWord.py
@@ -1,8 +1,9 @@
 def find_short(s):
-    l = len(s)
-    a = s.split()
+    l = len(s) #conta a quantidade caracteres apenas para ter um número alto o suficiente para comparação.
+    a = s.split() #separa as palavras
+    
     for x in a:
-        if l > len(x):
+        if l > len(x): #se l for maior que a quantidade de letras na palavra x, desse jeito l vai receber o valor menor sempre
             l = len(x)
         
     return l # l: shortest word length
