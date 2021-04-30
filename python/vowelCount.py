9a3fb182a4282dfd9160cd519e3ab813ab28a171
RenanTRS <renantrsouza@gmail.com>
2021-04-30 12:41:46 -0400

    Logic improvements

--- python/vowelCount.py
+++ python/vowelCount.py
@@ -1,6 +1,6 @@
 def get_count():
     num_vowels = 0
     for c in input_str:
-        if c == 'a' or c == 'e' or c == 'i' or c == 'o' or c == 'u':
+        if c in 'aeiou':
             num_vowels += 1
     return num_vowels
