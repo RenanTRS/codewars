d712d03f3bcad571a6d696aa3285f90166978c20
Renan Souza <renantrsouza@gmail.com>
2021-06-03 21:45:50 -0400

    Completed

--- python/mutiplicationTables.py
+++ python/mutiplicationTables.py
@@ -4,3 +4,19 @@
 #first column.
 
 
+def multiplication_table(row,col):
+    # Good Luck!
+    matriz = []
+    linha = []
+    for l in range(1, row+1):
+        for c in range(1, col+1):
+            linha.append(l * c)
+        matriz.append(linha[:])
+        linha.clear()
+    return matriz
+
+print(multiplication_table(2,2), [[1,2],[2,4]])
+print(multiplication_table(3,3), [[1,2,3],[2,4,6],[3,6,9]])
+print(multiplication_table(3,4), [[1,2,3,4],[2,4,6,8],[3,6,9,12]])
+print(multiplication_table(4,4), [[1,2,3,4],[2,4,6,8],[3,6,9,12],[4,8,12,16]])
+print(multiplication_table(2,5), [[1,2,3,4,5],[2,4,6,8,10]])
