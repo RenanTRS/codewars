731eb3028ebd1c8a0021e8e1eec1e439b8321e6f
Renan Souza <renantrsouza@gmail.com>
2021-06-03 21:14:56 -0400

    inital Commit

---
+++ python/mutiplicationTables.py
@@ -0,0 +1,6 @@
+#Create a function that accepts dimensions, of Rows x Columns, as parameters in order to create a multiplication table sized 
+#according to the given dimensions. **The return value of the function must be an array, and the numbers must be Fixnums, NOT 
+#strings.Each value on the table should be equal to the value of multiplying the number in its first row times the number in its 
+#first column.
+
+
