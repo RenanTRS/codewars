9b66c8efe2bfc578b7145ec265068ce4d745a814
Renan Souza <renantrsouza@gmail.com>
2021-06-22 21:38:32 -0400

    initial commit

---
+++ javaScript/shortestWord.js
@@ -0,0 +1,11 @@
+//Simple, given a string of words, return the length of the shortest word(s).
+//String will never be empty and you do not need to account for different data types.
+
+
+function findShort(s){
+    document.write('teste');
+}
+
+findShort("bitcoin take over the world maybe who knows perhaps");
+findShort("turns out random test cases are easier than writing out basic ones"); 
+findShort("Let's travel abroad shall we");
