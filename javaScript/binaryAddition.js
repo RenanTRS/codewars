a097928ad08fd7aa66e146d76a25006a16fe1fc7
Renan Souza <renantrsouza@gmail.com>
2021-06-26 16:51:59 -0400

    completed

--- javaScript/binaryAddition.js
+++ javaScript/binaryAddition.js
@@ -2,3 +2,22 @@
 can be done before, or after the addition. The binary number returned should be a string.*/
 
 
+function addBinary(a,b) {
+    var soma = a + b;
+    var listaResto = [];
+    var resto;
+    var numBin = '';
+    while(soma > 0){
+        resto = Math.floor(soma % 2);
+        listaResto.push(resto);
+        soma = Math.floor(soma / 2);
+    }
+
+    while(listaResto.length > 0){
+        numBin += listaResto.pop().toString();
+    }
+    
+    return numBin;
+}
+
+console.log(addBinary(1, 1));
